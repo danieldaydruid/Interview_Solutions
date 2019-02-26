708243dba973ef2b4bb89e7dae4ac409f5faf70b
anonymous <mattktechie@gmail.com>
2019-02-25 23:11:51 -0800

    C++ Questions added

--- C++/Challenge_2/main.cpp
+++ C++/Challenge_2/main.cpp
@@ -5,7 +5,7 @@
 /* PROMPT
 This problem was asked by Uber.
 
-Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i. 
 
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If your input was [3, 2, 1], the expected output would be [2, 3, 6]. 
 
