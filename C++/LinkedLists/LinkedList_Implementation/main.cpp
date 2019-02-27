d5e6858110c58e37a29784aeed8461d2e8a0531b
anonymous <mattktechie@gmail.com>
2019-02-27 13:39:04 -0800

    C++ LinkedList additions

--- C++/LinkedLists/LinkedList_Implementation/main.cpp
+++ C++/LinkedLists/LinkedList_Implementation/main.cpp
@@ -7,7 +7,7 @@ struct Node {
 };
 
 void Push(struct Node** head_ref, int new_data) {
-    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
     new_node->data = new_data;
     new_node->next = (*head_ref);
     (*head_ref) = new_node;
