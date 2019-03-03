c853e37ddbfa59be5bba91c24f88d50b5b57e4c9
anonymous <mattktechie@gmail.com>
2019-03-02 23:54:41 -0800

    C++ LinkedList additions

--- C++/LinkedLists/LinkedList_Implementation/main.cpp
+++ C++/LinkedLists/LinkedList_Implementation/main.cpp
@@ -29,7 +29,7 @@ void LinkedList::Push(int data) {
 void LinkedList::Print() { 
     Node* temp = Head; 
     while(temp != NULL) {
-        std::cout << temp->data << " "; 
+        std::cout << temp->data << " ";  
         temp = temp->next; 
     }  
     std::cout << std::endl; 
