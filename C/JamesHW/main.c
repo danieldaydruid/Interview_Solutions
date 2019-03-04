94eec0b6b19eadb7956f1beb687e20c77c25b28e
anonymous <mattktechie@gmail.com>
2019-03-03 22:25:30 -0800

    C malloccd buffer

--- C/JamesHW/main.c
+++ C/JamesHW/main.c
@@ -15,7 +15,7 @@ int main() {
     BoringBuffer[1] = malloc( (m + 1) * sizeof(char) );
     strcpy(BoringBuffer[1], "the");
     BoringBuffer[2] = malloc( (m + 1) * sizeof(char) );
-    strcpy(BoringBuffer[2], "a");
+    strcpy(BoringBuffer[2], "a"); 
     BoringBuffer[3] = malloc( (m + 1) * sizeof(char) );
     strcpy(BoringBuffer[3], "wolf");
     BoringBuffer[4] = malloc( (m + 1) * sizeof(char) );
